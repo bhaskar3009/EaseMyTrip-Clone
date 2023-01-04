63f12d581eedb2147b413e4a6afd86ea00d4e7fa
Bhaskar <bhaskarchavan2001@gmail.com>
2023-01-04 18:43:36 +0530

    upload folders

---
+++ footer/footer.js
@@ -0,0 +1,175 @@
+function footer() {
+	return `<div class="footer">
+    <div class="bottom_brdr">
+        <div class="dark_clr"></div>
+        <div class="light_clr"></div>
+    </div>
+    <div class="footer_inr">
+        <div class="ftr_col_1">
+            <div class="ftr_ttl"> Our Products </div>
+            <ul>
+                <li><a href="">Book
+                        Flights</a></li>
+                <li><a href="">Refer
+                        & Earn</a>
+                </li>
+                <li><a href="">Flight
+                        Status</a></li>
+                <li><a href="">Hotel
+                        Booking</a></li>
+                <li><a href="">Trains</a>
+                </li>
+                <li><a href="">Lowest
+                        Airfare Calendar</a></li>
+                <li><a href="">Domestic
+                        Flights</a></li>
+                <li><a href="">International
+                        Flights</a></li>
+                <li><a href="">Holiday
+                        Packages</a></li>
+                <!--li><a href="">Visa</a></li-->
+                <li><a href="">Bus</a>
+                </li>
+                <li><a href="">Cabs</a>
+                </li>
+                <li><a href="">Franchise</a>
+                </li>
+                <li><a href="" target="_new"><span>Mice</span></a>
+
+                </li>
+            </ul>
+        </div>
+        <div class="ftr_col_1">
+            <div class="ftr_ttl"> EMT Info </div>
+            <ul>
+                <li><a href="">Privacy
+                        Policy</a></li>
+                <li><a href="">Terms
+                        &amp; Conditions</a></li>
+                <li><a href="">User
+                        Agreement</a></li>
+                <li><a href="">Investors
+                        Relation</a></li>
+                <li><a href="">Mobile
+                        App</a></li>
+                <li><a href="">Achievements</a>
+                </li>
+                <li><a href="">Career</a>
+                </li>
+                <li><a href="">About
+                        Us</a></li>
+                <li><a href="">Contact
+                        Us</a></li>
+            </ul>
+        </div>
+        <div class="ftr_col_1">
+            <div class="ftr_ttl"> SITE DIRECTORY </div>
+            <ul>
+                <li><a href="">Flight
+                        by City</a></li>
+                <li><a href="">Holidays</a>
+                </li>
+                <li><a href="">Airlines</a>
+                </li>
+                <li><a href="">Airports</a>
+                </li>
+                <li><a href="">Flight
+                        Schedule</a></li>
+                <li><a href="">Train
+                        Routes</a></li>
+                <li><a href="">Bus
+                        Routes</a></li>
+                <li><a href="">Hotel
+                        Directory</a></li>
+                <li><a href="">Travel
+                        Guides</a></li>
+                <li><a href="">Blog</a>
+                </li>
+                <li><a href="">Travel
+                        Updates</a></li>
+                <li><a href="">Sitemap</a>
+                </li>
+                <li><a href="">Flight
+                        Offers</a></li>
+            </ul>
+        </div>
+        <div class="ftr_col_1">
+            <div class="ftr_ttl"> Popular Airlines </div>
+            <ul>
+                <li><a href="">IndiGo
+                        Airlines</a></li>
+                <li><a href="">Air
+                        India Airlines</a></li>
+                <li><a href="">GO
+                        FIRST Airlines</a></li>
+                <li><a href="">Vistara
+                        Airlines</a></li>
+                <li><a href="">SpiceJet
+                        Airlines</a></li>
+                <li><a href="">Air
+                        Asia Airlines</a></li>
+                <li><a href="">British
+                        airways</a></li>
+                <li><a href="">Qatar
+                        airways</a></li>
+                <li><a href="">Singapore
+                        Airlines</a></li>
+                <li><a href="">Etihad
+                        airways</a></li>
+                <li><a href="">Thai
+                        airways</a></li>
+                <li><a href="">United
+                        Airlines</a></li>
+                <li><a href="">American
+                        Airlines</a></li>
+                <li><a href="">Malaysia
+                        Airlines</a></li>
+            </ul>
+        </div>
+        <div class="ftr_col_2">
+            <div class="ftr_col_inr">
+                <div class="ftr_ttl">Payment & Security </div>
+                <div class="cards"><a class="visa" title="Visa"></a><a class="master" title="Master Card"></a><a
+                        class="american" title="American Express"></a>
+                    <a class="py_pal" title="PayPal"></a>
+                    <div class="clr"></div><a class="rupaylg" title="RuPay"></a><a class="pcilg"
+                        title="PCI Security"></a>
+                </div>
+                <div class="ftr_ttl">Follow us on </div>
+                <div class="social"><a href="https://www.facebook.com/EaseMyTrip/" target="_new" title="Facebook"
+                        class="fb_b"><span class="fb_n"></span></a>
+                    <a href="https://twitter.com/easemytrip" title="Twitter" target="_new" class="tw_b"><span
+                            class="tw_n"></span></a>
+                    <a href="https://www.youtube.com/c/EaseMyTripOfficial" title="You Tube" target="_new"
+                        class="yt_b"><span class="yt_n"></span></a>
+                    <a href="https://www.linkedin.com/company/easemytrip-com" title="LinkedIn" target="_new"
+                        class="li_b"><span class="li_n"></span></a>
+                    <a href="https://www.instagram.com/easemytrip/" title="Instagram" target="_new"
+                        class="in_b"><span class="in_n"></span></a>
+                </div>
+            </div>
+        </div>
+        <div class="ftr_col_3">
+            <div class="ap_d_cn"><span>Download</span>
+                EaseMyTrip App </div>
+            <a href="//play.google.com/store/apps/details?id=com.easemytrip.android"
+                class="pl_store hvr-wobble-horizontal" title="Google Play Store"></a>
+            <a href="https://itunes.apple.com/in/app/easemytrip-flight-booking/id1053030595?mt=8"
+                class="app_store hvr-wobble-horizontal" title="App Store"></a>
+        </div>
+    </div>
+    <div class="clr"></div>
+    <div class="brdr_1"></div>
+    <div class="footer_inr">
+        <div class="ftr_col_4">Copyright &copy; 2021 EaseMyTrip
+        </div>
+        <div class="ftr_col_5">
+            <ul>
+                <li><img src=""></li>
+            </ul>
+        </div>
+    </div>
+</div>`;
+}
+
+export default footer;
